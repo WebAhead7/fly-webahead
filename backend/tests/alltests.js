e2916dce4c92a5b2e0625c7d772a789874cef1a6
muhkham <muhkham92@gmail.com>
2020-11-04 23:10:45 +0200

    adding readme

--- backend/tests/alltests.js
+++ backend/tests/alltests.js
@@ -2,4 +2,23 @@ const test = require("tape");
 const supertest = require("supertest");
 const router = require("../router");
 
-test("TESTING ROUTE HOME", (t) => {});
+// test("TESTING ROUTE HOME", (t) => {});
+
+
+test("Initialise", t => {
+    let num = 2;
+    t.equal(num, 2, "Should return 2");
+    t.end();
+  });
+  
+  test("check status code is 200", t => {
+    supertest(router)
+      .get("/")
+      .expect(200)
+      .expect("Content-Type", "text/plain")
+      .end((err, res) => {
+        t.error(err);
+        t.equal(res.text, "Hello");
+        t.end();
+      });
+  });
